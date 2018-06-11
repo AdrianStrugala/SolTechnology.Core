e2663bf1af12700e318b7a082005bc378322f2f0
AdrianStrugala <strugala.adrian@gmail.com>
2018-06-11 18:06:10 +0200

    added streszczenie

---
+++ src/Authentication/Authentication/DreamAuthenticationOptions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Authentication.Authentication
+{
+    public class DreamAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        public string Key { get; set; }
+    }
+}
--- src/TESWebUI/Startup.cs
+++ src/TESWebUI/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TESWebUI.Authentication;
 
 namespace TESWebUI
 {
