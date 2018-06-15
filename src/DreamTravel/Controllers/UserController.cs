39f247ee54e449d449a448027d7a305eeab381c7
Adrian Strugala <adrian.strugala@comparex.com>
2018-06-15 15:27:29 +0200

    added Get user

--- src/DreamTravel/Controllers/UserController.cs
+++ src/DreamTravel/Controllers/UserController.cs
@@ -2,22 +2,20 @@
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using DapperExtensions;
 using DreamTravel.Models;
 
 namespace DreamTravel.Controllers
 {
     public class UserController : Controller
     {
-        private readonly DbConnectionFactory _dbConnectionFactory;
-
+        private readonly UserRepository _userRepository;
         public UserController(DbConnectionFactory dbConnectionFactory)
         {
-            _dbConnectionFactory = dbConnectionFactory;
+            _userRepository = new UserRepository(dbConnectionFactory);
         }
 
         [HttpPost]
-        public IActionResult Add(string name, string password)
+        public async Task<IActionResult> Add(string name, string password)
         {
             try
             {
@@ -28,11 +26,8 @@ namespace DreamTravel.Controllers
                     Happiness = 0
                 };
 
-                using (var connection = _dbConnectionFactory.CreateDbConnection())
-                {
-                    connection.Open();
-                    connection.Insert(toAdd);
-                }
+                await _userRepository.Add(toAdd);
+
             }
             catch (Exception e)
             {
@@ -40,6 +35,27 @@ namespace DreamTravel.Controllers
             }
 
             return Ok();
+        }
+
+        [HttpGet]
+        [Route("Get/{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            User user;
+            try
+            {
+                user = await _userRepository.Get(name);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Content(Newtonsoft.Json.JsonConvert.SerializeObject(user));
         }
     }
 }
