2dcd785a237d8396abef8121f2bbaf940c532f6b
AdrianStrugala <strugala.adrian@gmail.com>
2018-07-09 16:48:11 +0200

    fixed conversion error

--- src/DreamTravel/TSPControllerHandlers/CostLimitBreaker.cs
+++ src/DreamTravel/TSPControllerHandlers/CostLimitBreaker.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DreamTravel.Models;
 
 namespace DreamTravel.TSPControllerHandlers
 {
     public class CostLimitBreaker : ICostLimitBreaker
     {
+        private double conversionError = 0.000001;
         public List<Path> AdjustPaths(int costLimit, List<Path> paths)
         {
             paths.Sort((x, y) => 1 * x.Goal.CompareTo(y.Goal));
@@ -12,7 +14,7 @@ namespace DreamTravel.TSPControllerHandlers
             double overallCost = 0;
             foreach (var path in paths)
             {
-                if (path.Goal == 0) continue;
+                if (Math.Abs(path.Goal) < conversionError) continue;
                 if (overallCost + path.Cost <= costLimit)
                 {
                     overallCost += path.Cost;
