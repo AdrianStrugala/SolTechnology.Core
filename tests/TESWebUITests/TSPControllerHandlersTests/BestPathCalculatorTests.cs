ea781ba9c3156d61c6de43a06e4161bcf1226df3
Adrian Strugala <adrian.strugala@comparex.com>
2018-07-11 16:03:24 +0200

    Added happy path tests for TSP controller handlers

---
+++ tests/TESWebUITests/TSPControllerHandlersTests/BestPathCalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DreamTravel.ExternalConnection;
+using DreamTravel.Models;
+using DreamTravel.TSPControllerHandlers;
+using NSubstitute;
+using TravelingSalesmanProblem;
+using Xunit;
+
+namespace TESWebUITests.TSPControllerHandlersTests
+{
+    
+    public class BestPathCalculatorTests
+    {
+        private readonly IProcessInputData _processInputData;
+        private readonly IProcessOutputData _processOutputData;
+        private readonly ITSP _tspSolver;
+
+        private readonly BestPathCalculator _sut;
+        public BestPathCalculatorTests()
+        {
+            _processInputData = Substitute.For<IProcessInputData>();
+            _processOutputData = Substitute.For<IProcessOutputData>();
+            _tspSolver = Substitute.For<ITSP>();
+
+            _sut = new BestPathCalculator(_processInputData, _processOutputData, _tspSolver);
+        }
+
+        [Fact]
+        void Handle_ValidData_AllCallsAreDone()
+        {
+            //Arrange
+            _processInputData.ReadCities(Arg.Any<string>()).Returns(new List<string>());
+            _processInputData.GetCitiesFromGoogleApi(Arg.Any<List<string>>()).Returns(new List<City>());
+            _processInputData.FillMatrixWithData(Arg.Any<List<City>>(), Arg.Any<EvaluationMatrix>()).Returns(new EvaluationMatrix(1));
+            _tspSolver.SolveTSP(Arg.Any<double[]>()).Returns(new int[1]);
+            _processOutputData
+                .FormOutputFromTSPResult(Arg.Any<List<City>>(), Arg.Any<int[]>(), Arg.Any<EvaluationMatrix>())
+                .Returns(new List<Path>());
+
+            string listOfCities = "someValidListOfCities";
+
+            //Act
+            var result = _sut.Handle(listOfCities);
+
+            //Assert
+            Assert.NotNull(result);
+
+            _processInputData.Received(1).ReadCities(Arg.Any<string>());
+            _processInputData.Received(1).GetCitiesFromGoogleApi(Arg.Any<List<string>>());
+            _processInputData.Received(1).FillMatrixWithData(Arg.Any<List<City>>(), Arg.Any<EvaluationMatrix>());
+            _tspSolver.Received(1).SolveTSP(Arg.Any<double[]>());
+            _processOutputData.Received(1)
+                .FormOutputFromTSPResult(Arg.Any<List<City>>(), Arg.Any<int[]>(), Arg.Any<EvaluationMatrix>());
+        }
+    }
+}
---
+++ tests/TESWebUITests/TSPControllerHandlersTests/CostLimitBreakerTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using DreamTravel.Models;
+using DreamTravel.TSPControllerHandlers;
+using Xunit;
+
+namespace TESWebUITests.TSPControllerHandlersTests
+{
+    public class CostLimitBreakerTests
+    {
+        private readonly CostLimitBreaker _sut;
+        public CostLimitBreakerTests()
+        {
+            _sut = new CostLimitBreaker();
+        }
+
+        [Fact]
+        void Handle_ValidInput_ListOfCitiesIsSortedByIndex()
+        {
+            //Arrange
+            int costLimit = 40;
+            List<Path> paths = new List<Path>();
+            paths.Add(new Path()
+            {
+                Cost = 10,
+                Index = 0
+            });
+
+            paths.Add(new Path()
+            {
+                Cost = 15,
+                Index = 1
+            });
+
+            paths.Add(new Path()
+            {
+                Cost = 0,
+                Index = 2
+            });
+
+            paths.Add(new Path()
+            {
+                Cost = 83,
+                Index = 3
+            });
+
+
+            //Act
+            var result = _sut.Handle(costLimit, paths);
+
+            //Assert
+            Assert.Equal(0, result[0].Index);
+            Assert.Equal(1, result[1].Index);
+            Assert.Equal(2, result[2].Index);
+            Assert.Equal(3, result[3].Index);
+
+            double totalCost = 0;
+            foreach (var path in result)
+            {
+                totalCost += path.OptimalCost;
+            }
+
+            Assert.True(totalCost < costLimit);
+        }
+    }
+}
