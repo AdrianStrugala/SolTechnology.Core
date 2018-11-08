335087172f8dce78dabae13ec2ad345d58a30ff4
Adrian Strugala <strugala.adrian@gmail.com>
2018-11-08 19:52:11 +0100

    pictures are displaying

--- src/DreamTravel/wwwroot/js/Instruction.ts
+++ src/DreamTravel/wwwroot/js/Instruction.ts
@@ -17,9 +17,11 @@ function showSlides(n) {
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
