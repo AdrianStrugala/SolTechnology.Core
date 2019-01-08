ed29b69a0ff0ea6cb60d4c8ce0c735ce8fcdf301
Adrian Strugala <strugala.adrian@gmail.com>
2019-01-08 18:11:10 +0100

    refactor FindNameOfCity

--- src/DreamTravel/NameOfCity/FindNameOfCity.cs
+++ src/DreamTravel/NameOfCity/FindNameOfCity.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using DreamTravel.NameOfCity.Interfaces;
+using DreamTravel.SharedModels;
+using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
-using DreamTravel.NameOfCity.Interfaces;
-using DreamTravel.SharedModels;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace DreamTravel.NameOfCity
 {
@@ -30,34 +28,25 @@ namespace DreamTravel.NameOfCity
                 string url =
                     $"https://maps.googleapis.com/maps/api/geocode/json?latlng={city.Latitude},{city.Longitude}&sensor=false&key=AIzaSyBgCjCJuGQsXlAz6BUXPIL2_RSxgXUaCcM";
 
-                HttpResponseMessage getAsync = await _httpClient.GetAsync(url);
+                string response = await _httpClient.GetStringAsync(url);
+                JObject json = JObject.Parse(response);
 
-                using (Stream stream = getAsync.Content.ReadAsStreamAsync().Result ??
-                                       throw new ArgumentNullException(
-                                           $"Execption on [{MethodBase.GetCurrentMethod().Name}]"))
+                for (int i = 0; i < json["results"][0]["address_components"].Count(); i++)
                 {
-                    using (var jsonTextReader = new JsonTextReader(new StreamReader(stream)))
+                    for (int x = 0; x < json["results"][0]["address_components"][i]["types"].Count(); x++)
                     {
-                        JObject json = (JObject)new JsonSerializer().Deserialize(jsonTextReader);
-
-                        for (int i = 0; i < json["results"][0]["address_components"].Count(); i++)
+                        string type = json["results"][0]["address_components"][i]["types"][x].Value<string>();
+                        if (type == "locality" || type == "postal_town")
                         {
-                            for (int x = 0; x < json["results"][0]["address_components"][i]["types"].Count(); x++)
-                            {
-                                string type = json["results"][0]["address_components"][i]["types"][x].Value<string>();
-                                if (type == "locality" || type == "postal_town")
-                                {
-                                    city.Name = json["results"][0]["address_components"][i]["long_name"]
-                                        .Value<string>();
-                                }
-                            }
-                        }
-
-                        if (city.Name == null || city.Name.Equals(string.Empty))
-                        {
-                            city.Name = json["results"][0]["formatted_address"].Value<string>();
+                            city.Name = json["results"][0]["address_components"][i]["long_name"]
+                                .Value<string>();
                         }
                     }
+                }
+
+                if (city.Name == null || city.Name.Equals(string.Empty))
+                {
+                    city.Name = json["results"][0]["formatted_address"].Value<string>();
                 }
 
                 return city;
