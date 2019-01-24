32da5c66ab2f27b72dbff4341d2d23822f8e76e6
Adrian Strugala <strugala.adrian@gmail.com>
2019-01-24 17:19:21 +0100

    Add city by clicking on the map

---
+++ src/DreamTravel/wwwroot/js/AddCityByMapClickHandler.ts
@@ -0,0 +1,45 @@
+﻿function addCityByMapClickHandler(position, map) {
+
+    if (noOfCityRows > 1) {
+        addCityHandler(map);
+    }
+
+    displayCityAjaxCalls.push($.ajax({
+        type: 'POST',
+        dataType: 'html',
+        url: window.location + 'api/FindNameOfCity',
+        headers: {
+            'Authorization': 'DreamAuthentication U29sVWJlckFsbGVz'
+        },
+        data: { lat: position.lat(), lng: position.lng(), sessionId: sessionId },
+        success(msg) {
+
+            var city = JSON.parse(msg);
+
+
+            $("#listOfCities").children().each(function (index) {
+                if ($(this).attr('id') == "cityRow" + noOfCityRows.toString()) {
+
+                    if (markers[index] != null) {
+                        markers[index].setMap(null);
+                    }
+
+                    cities[index] = city;
+                    markers[index] = displayMarkerHandler(map,
+                        city.Latitude,
+                        city.Longitude,
+                        "✓");
+
+                    (<HTMLInputElement>$("#listOfCities").children().eq(index).children()[1]).value = city.Name;
+
+                    map.setCenter(markers[index].getPosition());
+                }
+            });
+        },
+
+        error(req, status, errorObj) {
+            var alertMessage = JSON.parse(req.responseText);
+            alert(alertMessage);
+        }
+    }));
+}
