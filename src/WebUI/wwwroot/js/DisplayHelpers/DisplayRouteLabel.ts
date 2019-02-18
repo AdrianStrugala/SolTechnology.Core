f1ff6c33f2fffeea2db1311a126e8ae4b62b0fd3
Adrian Strugala <strugala.adrian@gmail.com>
2019-02-18 19:10:01 +0100

    road label is finised - broken addCityByMapClick and DragAndDrop in the meantime

--- src/WebUI/wwwroot/js/DisplayHelpers/DisplayRouteLabel.ts
+++ src/WebUI/wwwroot/js/DisplayHelpers/DisplayRouteLabel.ts
@@ -2,6 +2,10 @@
 
 function displayRouteLabel(map, latitude, longtitude, text) {
 
+    var iconSize = new google.maps.Size(text.length * 0.6, 4, "em", "em");
+    var defaultFontSize = getDefaultFontSize();
+
+
     var marker = new google.maps.Marker(({
         position: {
             lat: latitude,
@@ -14,12 +18,24 @@ function displayRouteLabel(map, latitude, longtitude, text) {
             fontWeight: "bold"
         },
         icon: {
-          //  labelOrigin: new google.maps.Point(0, 0),
-          //  origin: new google.maps.Point(0, 0),
-            scaledSize: new google.maps.Size(30, 10, "em", "em"),
+            scaledSize: iconSize,
+            labelOrigin: new google.maps.Point(iconSize.width * (defaultFontSize * 0.8) / 2, iconSize.height * (defaultFontSize * 0.8)/2),
             url: "../images/labelFrame.jpg"
         }
     }) as any);
 
     return marker;
+}
+
+function getDefaultFontSize(): number {
+    var pa = document.body;
+    var who = document.createElement('div');
+
+    who.style.cssText = 'display:inline-block; padding:0; line-height:1; position:absolute; visibility:hidden; font-size:1em';
+
+    who.appendChild(document.createTextNode('M'));
+    pa.appendChild(who);
+    var fs = who.offsetWidth;
+    pa.removeChild(who);
+    return fs;
 }
