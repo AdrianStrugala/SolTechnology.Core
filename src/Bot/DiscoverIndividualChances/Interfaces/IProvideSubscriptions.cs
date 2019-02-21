6b7f6c8360fb2db254a04aa42898071952e18601
Adrian Strugala <adrian.strugala@comparex.com>
2019-02-21 10:31:52 +0100

    added code for reading subscriptions

---
+++ src/Bot/DiscoverIndividualChances/Interfaces/IProvideSubscriptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DreamTravel.Bot.DiscoverIndividualChances.Interfaces
+{
+    using Models;
+
+    public interface IProvideSubscriptions
+    {
+        List<Subscription> Execute();
+    }
+
+}
---
+++ src/Bot/DiscoverIndividualChances/SendEmail/ProvideSubscriptions.cs
@@ -0,0 +1,41 @@
+﻿namespace DreamTravel.Bot.DiscoverIndividualChances.SendEmail
+{
+    using Configuration;
+    using Dapper;
+    using Interfaces;
+    using Models;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ProvideSubscriptions : IProvideSubscriptions
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        const string Sql = @"
+SELECT 
+    Name AS UserName
+    ,From
+    ,To
+    ,NoOfMonthsFromNow
+FROM Subscription
+JOIN User on User.Id = UserId
+";
+
+        public ProvideSubscriptions(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public List<Subscription> Execute()
+        {
+            List<Subscription> result;
+
+            using (var connection = _dbConnectionFactory.CreateConnection())
+            {
+                result = connection.Query<Subscription>(Sql).ToList();
+            }
+
+            return result;
+        }
+    }
+}
