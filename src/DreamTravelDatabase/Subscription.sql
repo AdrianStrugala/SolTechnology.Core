118f233be691ce260cb16a7491d4f718b1a99de8
Adrian Strugala <adrian.strugala@comparex.com>
2019-02-22 09:16:48 +0100

    added subscription table to database

---
+++ src/DreamTravelDatabase/Subscription.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Subscription]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[UserId] INT NOT NULL, 
+	[From] NVARCHAR(50) NOT NULL, 
+	[To] NVARCHAR(50) NOT NULL, 
+	[NoOfMonthsFromNow] INT NOT NULL,
+	CONSTRAINT [FK_Subscription_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
