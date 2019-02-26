0a4cbcf34e51c2ff87d641cdf0ffddc29493173c
Adrian Strugala <adrian.strugala@comparex.com>
2019-02-26 15:10:18 +0100

    componse emial is a little more user friendly

--- src/Bot/DiscoverIndividualChances/SendEmail/ComposeMessage.cs
+++ src/Bot/DiscoverIndividualChances/SendEmail/ComposeMessage.cs
@@ -2,22 +2,23 @@
 {
     using Interfaces;
     using Models;
-    using Newtonsoft.Json;
 
     public class ComposeMessage : IComposeMessage
     {
         public string Execute(Chance chance, Subscription subscription)
         {
-            string message = $@"<html> <body> 
+            string message = $@"
+<html> 
+<body> 
 <h4> Hello {subscription.UserName}! </h4></br>
 </br>
 <p> Check out the details of your Dream Travel to {subscription.To}: </p></br>
-
-";
-
-            message += JsonConvert.SerializeObject(chance);
-
-            message += @"
+</br>
+<p>
+Price: {chance.Price} €
+From: {chance.Origin}, By: {chance.ThereCarrier}, At: {chance.ThereDay}
+To: {chance.Destination}, By: {chance.BackCarrier}, At: {chance.BackDay}
+</p>
 </br>
 </br>
 <p> Looks interesting? Check more details at: http://www.azair.eu/ </p></br>
