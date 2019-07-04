3b83b176fa25c2fbbe7fbdb6cfab08e6ecaa534f
Adrian Strugala <adrian.strugala@comparex.com>
2019-07-04 10:03:04 +0200

    removed instruction dots from the code

--- src/WebUI/wwwroot/js/Instruction.ts
+++ src/WebUI/wwwroot/js/Instruction.ts
@@ -15,7 +15,6 @@ function showSlides(n) {
     var i;
 
     var slides = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
 
@@ -23,9 +22,5 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
