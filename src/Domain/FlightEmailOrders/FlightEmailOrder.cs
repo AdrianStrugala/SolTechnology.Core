2636432c1274e2d0ade3bd4611ffcab85d6f1c3e
Adrian Strugala <ext.adrian.strugala@softwareone.com>
2019-09-17 15:03:16 +0200

    Added FlightEmailOrder domain model

---
+++ src/Domain/FlightEmailOrders/FlightEmailOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DreamTravel.Bot.DiscoverIndividualChances.Models
+{
+    public class FlightEmailOrder
+    {
+        public string UserName { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public DateTime DepartureDate { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+
+        public int MinDaysToStay { get; set; }
+
+        public int MaxDaysToStay { get; set; }
+
+        public bool OneWay { get; set; }
+
+        public string Email { get; set; }
+
+        public string Currency { get; set; }
+    }
+}
