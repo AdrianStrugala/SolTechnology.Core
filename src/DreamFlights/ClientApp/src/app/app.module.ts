10fac88635b89511efe250ddfa77cbc7df47565f
Adrian Strugala <strugala.adrian@gmail.com>
2019-10-06 17:28:15 +0200

    managed to connect angular component input values in 2 ways - next step is to invoke http request on submit

--- src/DreamFlights/ClientApp/src/app/app.module.ts
+++ src/DreamFlights/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FlightEmailOrderComponent } from './flight-email-order/flight-email-order.component'
@@ -20,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       BrowserAnimationsModule,
       HttpClientModule,
       FormsModule,
+      ReactiveFormsModule,
       MaterialModule,
       RouterModule.forRoot([
         { path: '', component: HomeComponent, pathMatch: 'full' }
--- src/DreamFlights/ClientApp/src/app/flight-email-order/flight-email-order.component.ts
+++ src/DreamFlights/ClientApp/src/app/flight-email-order/flight-email-order.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'flight-email-order',
   templateUrl: './flight-email-order.component.html',
   styleUrls: ['./flight-email-order.component.scss'],
 })
-export class FlightEmailOrderComponent {
 
+export class FlightEmailOrderComponent implements OnInit {
 
+  orderForm = new FormGroup({
+    from: new FormControl(),
+    to: new FormControl(),
+  });
+
+  onSubmit(): void {
+    console.log(this.orderForm.value); 
+  }
+
+  ngOnInit() {
+  }
 }
+
