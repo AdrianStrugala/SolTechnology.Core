0b6a29481a1c3eb1f290a1e0520b085f4343226f
Adrian Strugala <ext.adrian.strugala@softwareone.com>
2019-10-07 13:41:28 +0200

    hardcoded db connection string - have to decide how to handle application configuraiton

--- src/DatabaseData/Configuration/DatabaseDataConfiguration.cs
+++ src/DatabaseData/Configuration/DatabaseDataConfiguration.cs
@@ -2,6 +2,7 @@
 {
     public class DatabaseDataConfiguration
     {
-        public string ConnectionString { get; set; }     
+        public string ConnectionString =
+            "Server=tcp:dreamtravel.database.windows.net,1433;Initial Catalog=dreamtravel;Persist Security Info=False;User ID=adrian;Password=P4ssw0rd@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;";
     }
 }
--- src/DatabaseData/Configuration/ModuleInstaller.cs
+++ src/DatabaseData/Configuration/ModuleInstaller.cs
@@ -1,10 +1,7 @@
-﻿using System.IO;
-using System.Reflection;
-using DreamTravel.DatabaseData.FlightEmailOrders;
+﻿using DreamTravel.DatabaseData.FlightEmailOrders;
 using DreamTravel.DatabaseData.Subscriptions;
 using DreamTravel.DatabaseData.Users;
 using DreamTravel.Infrastructure.Database;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DreamTravel.DatabaseData.Configuration
@@ -13,16 +10,7 @@ namespace DreamTravel.DatabaseData.Configuration
     {
         public static IServiceCollection InstallDatabaseData(this IServiceCollection services)
         {
-            var configurationRoot = new ConfigurationBuilder()
-                                    //// SK: have no better idea how to do this. Feel free to tweak it :)
-                                    .AddJsonFile($"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\..\\databaseDataSettings.json", true)
-                                    .AddJsonFile("databaseDataSettings.json", true)
-                                    .Build();
-
             DatabaseDataConfiguration databaseDataConfiguration = new DatabaseDataConfiguration();
-            configurationRoot.Bind(databaseDataConfiguration);
-            services.AddSingleton<DatabaseDataConfiguration>(databaseDataConfiguration);
-
             services.AddSingleton<IDbConnectionFactory>(new DbConnectionFactory(databaseDataConfiguration.ConnectionString));
 
             services.AddTransient<IUserRepository, UserRepository>();
