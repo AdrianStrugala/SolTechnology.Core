68cf7950758d5b5d4d54442f2f093e056d8516bc
Adrian Strugala <strugala.adrian@gmail.com>
2019-10-07 19:26:34 +0200

    replaced db connection string with test one (for tests etc)

--- src/DatabaseData/Configuration/DatabaseDataConfiguration.cs
+++ src/DatabaseData/Configuration/DatabaseDataConfiguration.cs
@@ -3,6 +3,6 @@
     public class DatabaseDataConfiguration
     {
         public string ConnectionString =
-            "Server=tcp:dreamtravel.database.windows.net,1433;Initial Catalog=dreamtravel;Persist Security Info=False;User ID=adrian;Password=P4ssw0rd@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;";
+            "Server=tcp:dreamtravel.database.windows.net,1433;Initial Catalog=dreamtravel-demo;Persist Security Info=False;User ID=adrian;Password=P4ssw0rd@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;";
     }
 }
