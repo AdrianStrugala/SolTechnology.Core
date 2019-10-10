813697595628b8da7f4714eac032019c3c2232e0
Adrian Strugala <ext.adrian.strugala@softwareone.com>
2019-10-10 14:26:59 +0200

    moving RoutesApi backend in progress

--- src/GeolocationData/Matrices/MatrixRepository.GetCosts.cs
+++ src/GeolocationData/Matrices/MatrixRepository.GetCosts.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using DreamTravel.Domain.Cities;
-
-namespace DreamTravel.GeolocationData.Matrices
-{
-     public partial class MatrixRepository : IMatrixRepository
-    {
-        public Task<(double[], double[])> GetCosts(List<City> cities)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using DreamTravel.Domain.Cities;
+//
+//namespace DreamTravel.GeolocationData.Matrices
+//{
+//    public partial class MatrixRepository : IMatrixRepository
+//    {
+//        public Task<(double[], double[])> GetCosts(List<City> cities)
+//        {
+//            int matrixSize = cities.Count * cities.Count;
+//
+//            var result = (new double[matrixSize], new double[matrixSize]);
+//
+//            List<Task> tasks = new List<Task>();
+//
+//            for (int i = 0; i < matrixSize; i++)
+//            {
+//                for (int j = 0; j < matrixSize; j++)
+//                {
+//                    int iterator = j + i * matrixSize;
+//
+//                    var i1 = i;
+//                    var j1 = j;
+//                    tasks.Add(Task.Run(async () => (evaluationMatrix.Costs[iterator], evaluationMatrix.VinietaCosts[iterator]) =
+//                                                   await _downloadCostBetweenTwoCities.Execute(cities[i1], cities[j1])));
+//                }
+//            }
+//
+//            return tasks;
+//        }
+//
+//        private async Task<(double, double)> DownloadCostBetweenTwoCities(City origin, City destination)
+//        {
+//
+//        }
+//
+//    }
+//}
