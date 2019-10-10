a1488e41b83fdcb3c7cf4d713d155cf75d2e2799
Adrian Strugala <ext.adrian.strugala@softwareone.com>
2019-10-10 12:43:53 +0200

    redability fix

--- src/FlightsRepository/Airports/AirportRepository.GetCodesByPlace.cs
+++ src/FlightsRepository/Airports/AirportRepository.GetCodesByPlace.cs
@@ -13,10 +13,14 @@ namespace DreamTravel.FlightProviderData.Airports
 
             foreach (KeyValuePair<string, string> cityToCode in cityToCodes)
             {
-                if (!combinedResult.TryAdd(cityToCode.Key, new List<string>
+                if (!combinedResult.ContainsKey(cityToCode.Key))
                 {
-                    cityToCode.Value
-                }))
+                    combinedResult.Add(cityToCode.Key, new List<string>
+                    {
+                        cityToCode.Value
+                    });
+                }
+                else
                 {
                     combinedResult.Add($"{cityToCode.Key} ({cityToCode.Value})", new List<string>
                     {
