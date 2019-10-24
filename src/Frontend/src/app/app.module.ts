c9856c9706e5c3be2618814dce1e9984a672d90d
Adrian Strugala <ext.adrian.strugala@softwareone.com>
2019-10-24 15:17:23 +0200

    added sketches of login component and service (how to put it together?)

--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialModule } from './material-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
         NavbarComponent,
-        FlightEmailOrderComponent
+        FlightEmailOrderComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
