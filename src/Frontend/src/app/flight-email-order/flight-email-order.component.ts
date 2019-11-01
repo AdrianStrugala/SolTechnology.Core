e127bdb4dc61ce51d4901dbac66670d20ebbd341
Adrian Strugala <strugala.adrian@gmail.com>
2019-11-01 19:50:08 +0100

    validation on login page is working :)

--- src/Frontend/src/app/flight-email-order/flight-email-order.component.ts
+++ src/Frontend/src/app/flight-email-order/flight-email-order.component.ts
@@ -29,7 +29,7 @@ export class FlightEmailOrderComponent implements OnInit {
 
     constructor(private http: HttpClient, private userService : UserService) { }
 
-    url = "http://localhost:53725/api/OrderFlightEmail";
+    url = "https://dreamtravelsapi-demo.azurewebsites.net/api/OrderFlightEmail";
 
     orderForm = new FormGroup({
         from: new FormControl(),
