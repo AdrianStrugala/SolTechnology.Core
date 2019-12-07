4afa4d6d9a5e088f33d02a14649521f767fad72d
Adrian Strugala <strugala.adrian@gmail.com>
2019-12-07 17:43:56 +0100

    fixed build errors, register success and error handled

--- src/Frontend/src/app/main-page/home/home.component.ts
+++ src/Frontend/src/app/main-page/home/home.component.ts
@@ -1,18 +1,31 @@
 import { ComponentRef, ComponentFactoryResolver, ViewContainerRef, ViewChild, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent {
   title = 'Dream Flights';
 
   @ViewChild('viewContainerRef', { read: ViewContainerRef, static: null }) VCR: ViewContainerRef;
 
-
+  success: string;
   // to store references of dynamically created components
   componentsReferences = [];
 
-  constructor(private CFR: ComponentFactoryResolver) {
+  page: number;
+
+  constructor(private CFR: ComponentFactoryResolver,private route: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit() {
+   this.route
+      .queryParams
+      .subscribe(params => {
+        // Defaults to 0 if no query param provided.
+        this.success = params['successMessage'];
+      });
   }
 }
--- src/Frontend/src/app/register/register.component.ts
+++ src/Frontend/src/app/register/register.component.ts
@@ -1,50 +1,46 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { UserService, IUser } from '../user.service';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { UserService, IUser } from "../user.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
-export class RegisterComponent{
-
+export class RegisterComponent {
   registerForm = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl("", {
       validators: [Validators.required, Validators.email],
       updateOn: "blur"
     }),
-    name: new FormControl(
-      null,
-      { validators: [Validators.required] }
-    ),
-    password: new FormControl(
-      null,
-      { validators: [Validators.required] }
-    ),
-    confirmPassword: new FormControl(
-      null,
-      { validators: [Validators.required] }
-    ),
+    name: new FormControl(null, { validators: [Validators.required] }),
+    password: new FormControl(null, { validators: [Validators.required] }),
+    confirmPassword: new FormControl(null, {
+      validators: [Validators.required]
+    })
   });
 
-  error : string;
+  error: string;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) {}
 
-  register(){
-    let user: IUser;
+  register() {
+    this.error = null;
+
+    var user = {} as IUser;
+    console.log(this.registerForm.value);
     user.email = this.registerForm.value.email;
     user.password = this.registerForm.value.password;
     user.name = this.registerForm.value.name;
 
-    this.userService.register(user)
-    .subscribe(
-      (data : boolean) => {        
-    
-    },
-    (error) => {
-      this.error = error.error
-    });
+    this.userService.register(user).subscribe(
+      () => {
+        this.router.navigate([""], { queryParams: { successMessage: "You are successfully registered!" } })
+      },
+      error => {
+        this.error = error.error;
+      }
+    );
   }
 }
