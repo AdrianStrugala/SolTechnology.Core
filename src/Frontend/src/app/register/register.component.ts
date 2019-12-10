995a7415f674a91e64e4b63f047ab68cc7bd6d56
Adrian Strugala <strugala.adrian@gmail.com>
2019-12-10 18:28:23 +0100

    added validation on registration

--- src/Frontend/src/app/register/register.component.ts
+++ src/Frontend/src/app/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn
+} from "@angular/forms";
 import { UserService, IUser } from "../user.service";
 import { Router } from "@angular/router";
 import { SuccessMessageService } from "../main-page/success-message/success-message.service";
@@ -10,21 +15,44 @@ import { SuccessMessageService } from "../main-page/success-message/success-mess
   styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    email: new FormControl("", {
-      validators: [Validators.required, Validators.email],
-      updateOn: "blur"
-    }),
-    name: new FormControl(null, { validators: [Validators.required] }),
-    password: new FormControl(null, { validators: [Validators.required] }),
-    confirmPassword: new FormControl(null, {
-      validators: [Validators.required]
-    })
-  });
+  confirmPasswordValidator: ValidatorFn = (orderForm: FormGroup) => {
+    let password = orderForm.get("password").value;
+    let confirmPassword = orderForm.get("confirmPassword").value;
+
+    if (password != confirmPassword) {
+      return { confirmPassword };
+    }
+    return null;
+  };
+
+  registerForm = new FormGroup(
+    {
+      email: new FormControl("", {
+        validators: [Validators.required, Validators.email],
+        updateOn: "blur"
+      }),
+      name: new FormControl(null, { validators: [Validators.required] }),
+      password: new FormControl(null, {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(100)
+        ]
+      }),
+      confirmPassword: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    },
+    [this.confirmPasswordValidator]
+  );
 
   error: string;
 
-  constructor(private userService: UserService, private router: Router, private successMessageService: SuccessMessageService) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private successMessageService: SuccessMessageService
+  ) {}
 
   register() {
     this.error = null;
@@ -38,7 +66,7 @@ export class RegisterComponent {
     this.userService.register(user).subscribe(
       () => {
         this.successMessageService.set("You are successfully registered!");
-        this.router.navigate([""])
+        this.router.navigate([""]);
       },
       error => {
         this.error = error.error;
