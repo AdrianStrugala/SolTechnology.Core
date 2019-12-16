294eae41fd8c142272c7dfbbd7921f4e88d5ae1b
Adrian <strugala.adrian@gmail.com>
2019-12-16 15:20:03 +0100

    flightEmailOrders$ are displaying using observable :O

--- src/Frontend/src/app/my-account-page/flight-order-list/flight-order-list.component.ts
+++ src/Frontend/src/app/my-account-page/flight-order-list/flight-order-list.component.ts
@@ -1,32 +1,20 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { UserService } from '../../user.service';
-import { FlightOrderListService, IFlightEmailOrder } from './flight-order-list.service'  
-  
-  @Component({
-    selector: 'flight-order-list',
-    templateUrl: './flight-order-list.component.html',
-    styleUrls: ['./flight-order-list.component.scss']
-  })
-  export class FlightOrderListComponent implements AfterViewInit {
-  
-    public flightEmailOrders: IFlightEmailOrder[];
-  
-  
-    constructor(private userService: UserService, private flightEmailOrderService: FlightOrderListService) { }
-  
-    ngAfterViewInit() {
-  
-      console.log(this.userService.user)
-  
-      this.flightEmailOrderService.GetFlightEmailOrdersForUser(this.userService.user.id)
-        .subscribe(
-          (data: IFlightEmailOrder[]) => {
-            this.flightEmailOrders = data;
-  
-            console.log(data)
-          },
-          (error) => {
-            //ignore
-          });
-    }
+import { FlightOrderListService, IFlightEmailOrder } from './flight-order-list.service'
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'flight-order-list',
+  templateUrl: './flight-order-list.component.html',
+  styleUrls: ['./flight-order-list.component.scss']
+})
+export class FlightOrderListComponent implements AfterViewInit {
+
+  public flightEmailOrders$: Observable<IFlightEmailOrder[]>;
+
+  constructor(private userService: UserService, private flightEmailOrderService: FlightOrderListService) { }
+
+  ngAfterViewInit() {
+    this.flightEmailOrders$ = this.flightEmailOrderService.GetFlightEmailOrdersForUser(this.userService.user.id);
+  }
 }
