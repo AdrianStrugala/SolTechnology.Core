9152b929a3c2d677e612f1b0faaafcd1ae29d708
Adrian Strugala <strugala.adrian@gmail.com>
2019-12-17 18:09:21 +0100

    switch of tabs works

--- src/Frontend/src/app/main-page/navbar/navbar.component.ts
+++ src/Frontend/src/app/main-page/navbar/navbar.component.ts
@@ -1,30 +1,51 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { LoginComponent } from '../login/login.component';
-import { UserService } from '../../user.service';
+import { Component } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { LoginComponent } from "../login/login.component";
+import { UserService } from "../../user.service";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: "navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
+export class NavbarComponent {
+  navLinks: { label: string; link: string; index: number; }[];
+  activeLinkIndex = -1;
 
-export class NavbarComponent{
 
-    constructor(public dialog: MatDialog, public userService: UserService) { }
+  constructor(public dialog: MatDialog, public userService: UserService, private router: Router) {
+    this.navLinks = [
+      {
+          label: 'TabTest1',
+          link: './my-account',
+          index: 0
+      }, {
+          label: 'Tab Test2',
+          link: '',
+          index: 1
+      }, {
+          label: 'Tab Test3',
+          link: './register',
+          index: 2
+      },
+  ];
+  }
 
-    openDialog(): void {
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+        this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
 
-        if(this.dialog.openDialogs.length == 0){
-            this.dialog.open(LoginComponent, {
-                height: '35rem',
-                width: '30rem'
-              });
-        }
-        else{
-            this.dialog.closeAll();
-        }
+  openDialog(): void {
+    if (this.dialog.openDialogs.length == 0) {
+      this.dialog.open(LoginComponent, {
+        height: "35rem",
+        width: "30rem"
+      });
+    } else {
+      this.dialog.closeAll();
     }
+  }
 }
-  
-
