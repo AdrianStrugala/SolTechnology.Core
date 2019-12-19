d80d46e522def5cfa876293a3d6379d92fc5c780
Adrian Strugala <strugala.adrian@gmail.com>
2019-12-19 20:33:37 +0100

    problem with async validator (as always) xd

--- src/Frontend/src/app/dream-flights-page/flight-email-order/airports.service.ts
+++ src/Frontend/src/app/dream-flights-page/flight-email-order/airports.service.ts
@@ -1,22 +1,32 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { publishReplay, map, refCount } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AirportsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  airports$: Observable<IAirport[]> = this.http
+    .get<IAirport[]>(
+      "https://dreamtravelsapi-demo.azurewebsites.net/api/airports"
+    )
+    .pipe(
+      map(val => val || []),
+      publishReplay(1),
+      refCount()
+    );
 
   getAirports(): Observable<{}> {
-    return this.http.get("https://dreamtravelsapi-demo.azurewebsites.net/api/airports");
+    return this.http.get(
+      "https://dreamtravelsapi-demo.azurewebsites.net/api/airports"
+    );
   }
 }
 
-
-
-export interface IAirport { 
+export interface IAirport {
   id: number;
   name: string;
   codes: string[];
--- src/Frontend/src/app/dream-flights-page/flight-email-order/flight-email-order.component.ts
+++ src/Frontend/src/app/dream-flights-page/flight-email-order/flight-email-order.component.ts
@@ -1,175 +1,212 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormBuilder, ValidatorFn } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { UserService } from '../../user.service';
-import { IAirport, AirportsService } from './airports.service';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-import { SuccessMessageService } from '../../main-page/success-message/success-message.service';
-import { Router } from '@angular/router';
-import { handleError } from '../../shared/error';
-
+import { Component, OnInit, Inject } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+  ValidatorFn,
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors
+} from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS
+} from "@angular/material-moment-adapter";
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from "@angular/material/core";
+import { UserService } from "../../user.service";
+import { IAirport, AirportsService } from "./airports.service";
+import { Observable, combineLatest, of } from "rxjs";
+import { map, startWith, switchMap, filter, pluck, tap } from "rxjs/operators";
+import { SuccessMessageService } from "../../main-page/success-message/success-message.service";
+import { Router } from "@angular/router";
+import { handleError } from "../../shared/error";
 
 @Component({
-    selector: 'flight-email-order',
-    templateUrl: './flight-email-order.component.html',
-    styleUrls: ['./flight-email-order.component.scss'],
+  selector: "flight-email-order",
+  templateUrl: "./flight-email-order.component.html",
+  styleUrls: ["./flight-email-order.component.scss"],
 
-    providers: [
-        { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
-        // The locale would typically be provided on the root module of your application. We do it at
-        // the component level here, due to limitations of our example generation script.
-        { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+  providers: [
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+    // The locale would typically be provided on the root module of your application. We do it at
+    // the component level here, due to limitations of our example generation script.
+    { provide: MAT_DATE_LOCALE, useValue: "en-GB" },
 
-        // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
-        // `MatMomentDateModule` in your applications root module. We provide it at the component level
-        // here, due to limitations of our example generation script.
-        { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS] },
-        { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
-    ]
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS }
+  ]
 })
-
 export class FlightEmailOrderComponent implements OnInit {
+  airportsSubscription: any;
+  constructor(
+    private http: HttpClient,
+    private userService: UserService,
+    private ariportService: AirportsService,
+    private successMessageService: SuccessMessageService,
+    private router: Router
+  ) {}
 
-    constructor(
-        private http: HttpClient,
-        private userService: UserService,
-        private ariportService: AirportsService,
-        private successMessageService: SuccessMessageService,
-        private router: Router
-    ) { }
+  url = "https://dreamtravelsapi-demo.azurewebsites.net/api/OrderFlightEmail";
 
-    url = "https://dreamtravelsapi-demo.azurewebsites.net/api/OrderFlightEmail";
+  error: string;
+  airports: IAirport[];
+  autocomplete: string[];
 
-    error: string;
-    airports: IAirport[];
-    autocomplete: string[];
-    filteredFrom: Observable<string[]>;
-    filteredTo: Observable<string[]>;
+  minDaysValidator: ValidatorFn = (orderForm: FormGroup) => {
+    let minDays = orderForm.get("minDaysOfStay").value;
+    let arrivalDate = orderForm.get("arrivalDate").value;
+    let departureDate = orderForm.get("departureDate").value;
 
-    minDaysValidator: ValidatorFn = (orderForm: FormGroup) => {
-        let minDays = orderForm.get('minDaysOfStay').value;
-        let arrivalDate = orderForm.get('arrivalDate').value;
-        let departureDate = orderForm.get('departureDate').value;
+    let differenceInTime = arrivalDate - departureDate;
+    var differenceInDays = differenceInTime / (1000 * 3600 * 24);
 
-        let differenceInTime = arrivalDate - departureDate;
-        var differenceInDays = differenceInTime / (1000 * 3600 * 24);
-
-        if (minDays > differenceInDays) {
-            return { minDays }
-        }
-        return null;
+    if (minDays > differenceInDays) {
+      return { minDays };
     }
+    return null;
+  };
 
+  arrivalDateValidator: ValidatorFn = (orderForm: FormGroup) => {
+    let arrivalDate = orderForm.get("arrivalDate").value;
+    let departureDate = orderForm.get("departureDate").value;
 
-    autocompleteValidator: ValidatorFn = (control: FormControl) => {
+    if (departureDate > arrivalDate) {
+      return { arrivalDate };
+    }
+    return null;
+  };
 
-        if (control.value == null || control.value == "") {
+  maxDaysValidator: ValidatorFn = (orderForm: FormGroup) => {
+    let minDays = orderForm.get("minDaysOfStay").value;
+    let maxDays = orderForm.get("maxDaysOfStay").value;
+
+    if (minDays > maxDays) {
+      return { maxDays };
+    }
+    return null;
+  };
+
+  orderForm = new FormGroup(
+    {
+      from: new FormControl("", {
+        validators: [Validators.required],
+        asyncValidators: [this.usernameValidator2()]
+      }),
+      to: new FormControl("", {
+        validators: [Validators.required]
+      }),
+      departureDate: new FormControl(new Date(), {
+        validators: [Validators.required]
+      }),
+      arrivalDate: new FormControl("", {
+        validators: [Validators.required]
+      }),
+      minDaysOfStay: new FormControl("", {
+        validators: [Validators.required, Validators.min(1)],
+        updateOn: "blur"
+      }),
+      maxDaysOfStay: new FormControl("", {
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      userId: new FormControl()
+    },
+    [this.minDaysValidator, this.arrivalDateValidator, this.maxDaysValidator]
+  );
+
+  filteredFrom$: Observable<IAirport[]> = combineLatest([
+    this.orderForm.get("from").valueChanges,
+    this.ariportService.airports$
+  ]).pipe(
+    map(([from, airports]) =>
+      airports.filter(a => a.name.toLowerCase().includes(from.toLowerCase()))
+    )
+  );
+
+  usernameValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      return this.filteredFrom$.pipe(
+        map(airports => {
+          console.log(airports.length);
+          if (airports.length == 0) {
+            return { autocomplete: true };
+          } else {
             return null;
+          }
+          // do stuff, and either return error or "of(null)"
+        })
+      );
+    };
+  }
+
+  usernameValidator2(): AsyncValidatorFn {
+    return (control: AbstractControl): Promise<ValidationErrors | null> => {
+      return this.filteredFrom$.toPromise().then(airports => {
+        return airports.length != 0 ? null : { autocomplete: true };
+      });
+    };
+  }
+
+  UniqueNameValidator(ctrl: AbstractControl) {}
+
+  // autocompleteValidator1: ValidatorFn = (control: FormControl) => {
+  //   if (control.value == null || control.value == "") {
+  //     return null;
+  //   }
+  //   this.filteredFrom.subscribe(r => {
+  //     if (r.length == 0) return { autocomplete: true };
+  //   });
+  //   // if (this._filter(control.value).length == 0) {
+  //   //   return { autocomplete: true };
+  //   // }
+  //   return null;
+  // };
+
+  filteredTo$: Observable<IAirport[]> = combineLatest([
+    this.orderForm.get("to").valueChanges,
+    this.ariportService.airports$
+  ]).pipe(
+    tap(x => console.log(x)),
+    map(([from, airports]) =>
+      airports.filter(a => a.name.toLowerCase().includes(from.toLowerCase()))
+    ),
+    tap(x => console.log(x))
+  );
+
+  minDepartureDate = new Date();
+  minArrivalDate = this.orderForm.value.departureDate;
+
+  ngOnInit() {}
+
+  onSubmit(): void {
+    this.error = null;
+    this.orderForm.value.userId = this.userService.user.id;
+
+    this.http
+      .post(this.url, this.orderForm.value, {
+        observe: "body"
+      })
+      .subscribe(
+        () => {
+          this.successMessageService.set("Order successfully placed!");
+          this.router.navigate([""]);
+        },
+        error => {
+          this.error = handleError(error);
         }
-        if (this._filter(control.value).length == 0) {
-            return { autocomplete: true }
-        }
-        return null;
-    }
-
-    arrivalDateValidator: ValidatorFn = (orderForm: FormGroup) => {
-        let arrivalDate = orderForm.get('arrivalDate').value;
-        let departureDate = orderForm.get('departureDate').value;
-
-        if (departureDate > arrivalDate) {
-            return { arrivalDate }
-        }
-        return null;
-    }
-
-    maxDaysValidator: ValidatorFn = (orderForm: FormGroup) => {
-        let minDays = orderForm.get('minDaysOfStay').value;
-        let maxDays = orderForm.get('maxDaysOfStay').value;
-
-        if (minDays > maxDays) {
-            return { maxDays }
-        }
-        return null;
-    }
-
-    orderForm = new FormGroup({
-        from: new FormControl('', {
-            validators: [Validators.required, this.autocompleteValidator]
-        }),
-        to: new FormControl('', {
-            validators: [Validators.required, this.autocompleteValidator]
-        }),
-        departureDate: new FormControl(new Date(), {
-            validators: [Validators.required]
-        }),
-        arrivalDate: new FormControl('', {
-            validators: [Validators.required]
-        }),
-        minDaysOfStay: new FormControl('', {
-            validators: [Validators.required, Validators.min(1)]
-            , updateOn: "blur"
-        }),
-        maxDaysOfStay: new FormControl('', {
-            validators: [Validators.required, Validators.min(1)]
-        }),
-        userId: new FormControl
-    }, [this.minDaysValidator, this.arrivalDateValidator, this.maxDaysValidator]);
-
-    minDepartureDate = new Date();
-    minArrivalDate = this.orderForm.value.departureDate;
-
-
-    ngOnInit() {
-        this.ariportService.getAirports().subscribe(
-            (data: IAirport[]) => {
-                this.airports = data;
-                this.autocomplete = this.airports.map(a => a.name);
-
-                //from autocomplete
-                this.filteredFrom = this.orderForm.get('from').valueChanges
-                    .pipe(
-                        startWith(''),
-                        map(value => this._filter(value))
-                    );
-
-                //to autocomplete
-                this.filteredTo = this.orderForm.get('to').valueChanges
-                    .pipe(
-                        startWith(''),
-                        map(value => this._filter(value))
-                    );
-            }
-        )
-    }
-
-    private _filter(value: string): string[] {
-        const filterValue = value.toLowerCase();
-        return this.autocomplete.filter(option => option.toLowerCase().includes(filterValue));
-    }
-
-    onSubmit(): void {
-
-        this.error = null;
-        this.orderForm.value.userId = this.userService.user.id
-
-        this.http.post(
-            this.url,
-            this.orderForm.value,
-            {
-                observe: "body"
-            })
-            .subscribe(
-                () => {
-                    this.successMessageService.set("Order successfully placed!");
-                    this.router.navigate([""])
-                },
-                error => {
-                    this.error = handleError(error);
-                }
-            );
-    }
+      );
+  }
 }
-
