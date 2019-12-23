dec5faaf178f03b19c494b06a7557a3d5fa44481
Adrian Strugala <strugala.adrian@gmail.com>
2019-12-23 13:25:33 +0100

    fixed display of orders

--- src/Frontend/src/app/my-account-page/flight-order-list/flight-order-list.component.ts
+++ src/Frontend/src/app/my-account-page/flight-order-list/flight-order-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { UserService } from '../../user.service';
 import { FlightOrderListService, IFlightEmailOrder } from './flight-order-list.service'
 import { Observable } from 'rxjs';
@@ -9,13 +9,10 @@ import { Observable } from 'rxjs';
   styleUrls: ['./flight-order-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FlightOrderListComponent implements AfterViewInit {
+export class FlightOrderListComponent {
 
-  public flightEmailOrders$: Observable<IFlightEmailOrder[]>;
+  public flightEmailOrders$: Observable<IFlightEmailOrder[]> = this.flightEmailOrderService.GetFlightEmailOrdersForUser(this.userService.user.id);
 
   constructor(private userService: UserService, private flightEmailOrderService: FlightOrderListService) { }
 
-  ngAfterViewInit() {
-    this.flightEmailOrders$ = this.flightEmailOrderService.GetFlightEmailOrdersForUser(this.userService.user.id);
-  }
 }
