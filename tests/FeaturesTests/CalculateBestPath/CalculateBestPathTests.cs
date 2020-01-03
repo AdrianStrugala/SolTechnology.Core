12383c6b211ddcc3e039bd34a5cd7fa5dc2c6c0e
Adrian Strugala <strugala.adrian@gmail.com>
2020-01-03 12:57:46 +0100

    removed old references to already ported endpoints

--- tests/FeaturesTests/CalculateBestPath/CalculateBestPathTests.cs
+++ tests/FeaturesTests/CalculateBestPath/CalculateBestPathTests.cs
@@ -39,7 +39,7 @@ namespace DreamTravel.FeaturesTests.CalculateBestPath
             List<City> cities = new List<City> { new City { Name = "Wroclaw", Latitude = 21, Longitude = 37 } };
             
             //Act
-            var result = _sut.Execute(cities, true);
+            var result = _sut.Execute(cities);
 
 
             //Assert
@@ -50,32 +50,32 @@ namespace DreamTravel.FeaturesTests.CalculateBestPath
             _tspSolver.Received(1).SolveTSP(Arg.Any<List<double>>());
         }
 
-        [Fact]
-        async Task Handle_DoNotOptimizePath_ResultOrderIsTheSameAsInput()
-        {
-            //Arrange
-            _downloadRoadData.Execute(Arg.Any<List<City>>(), Arg.Any<EvaluationMatrix>()).Returns(new EvaluationMatrix(3));
-            _evaluationBrain.Execute(Arg.Any<EvaluationMatrix>(), Arg.Any<int>()).Returns(new EvaluationMatrix(3));
-            List<int> order = new List<int> { 0, 2, 1 };
-            _tspSolver.SolveTSP(Arg.Any<List<double>>()).Returns(order);
-
-            List<City> cities = new List<City>
-            {
-                new City { Name = "Wroclaw", Latitude = 21, Longitude = 37 },
-                new City { Name = "WroclawNorth", Latitude = 22, Longitude = 37 },
-                new City { Name = "WroclawSouth", Latitude = 20, Longitude = 37 }
-            };
-            
-            //Act
-            var result = await _sut.Execute(cities, false);
-            var bestPaths = result.BestPaths;
-
-
-            //Assert
-            Assert.NotNull(result);
-            Assert.Equal(cities[0], bestPaths[0].StartingCity);
-            Assert.Equal(cities[1], bestPaths[1].StartingCity);
-            Assert.Equal(cities[2], bestPaths[1].EndingCity);
-        }
+//        [Fact]
+//        async Task Handle_DoNotOptimizePath_ResultOrderIsTheSameAsInput()
+//        {
+//            //Arrange
+//            _downloadRoadData.Execute(Arg.Any<List<City>>(), Arg.Any<EvaluationMatrix>()).Returns(new EvaluationMatrix(3));
+//            _evaluationBrain.Execute(Arg.Any<EvaluationMatrix>(), Arg.Any<int>()).Returns(new EvaluationMatrix(3));
+//            List<int> order = new List<int> { 0, 2, 1 };
+//            _tspSolver.SolveTSP(Arg.Any<List<double>>()).Returns(order);
+//
+//            List<City> cities = new List<City>
+//            {
+//                new City { Name = "Wroclaw", Latitude = 21, Longitude = 37 },
+//                new City { Name = "WroclawNorth", Latitude = 22, Longitude = 37 },
+//                new City { Name = "WroclawSouth", Latitude = 20, Longitude = 37 }
+//            };
+//            
+//            //Act
+//            var result = await _sut.Execute(cities, false);
+//            var bestPaths = result.BestPaths;
+//
+//
+//            //Assert
+//            Assert.NotNull(result);
+//            Assert.Equal(cities[0], bestPaths[0].StartingCity);
+//            Assert.Equal(cities[1], bestPaths[1].StartingCity);
+//            Assert.Equal(cities[2], bestPaths[1].EndingCity);
+//        }
     }
 }
