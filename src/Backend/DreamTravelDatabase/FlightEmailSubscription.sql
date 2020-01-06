ae0e2f6967cf2428527c04c55e1f65c8b84ebbdd
Adrian Strugala <strugala.adrian@gmail.com>
2020-01-06 19:46:52 +0100

    added table FlightEmailSubscription - after deployment sqls have to be changed

---
+++ src/Backend/DreamTravelDatabase/FlightEmailSubscription.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[FlightEmailSubscription]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[UserId] INT NOT NULL, 
+	[From] NVARCHAR(50) NOT NULL, 
+	[To] NVARCHAR(50) NOT NULL, 
+	[DepartureDate] DATETIME2 NOT NULL, 
+	[ArrivalDate] DATETIME2 NOT NULL, 
+	[MinDaysOfStay] INT NOT NULL,
+	[MaxDaysOfStay] INT NOT NULL,
+	[OneWay] BIT NOT NULL default 0
+
+    CONSTRAINT [FK_FlightEmailSubscription_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
