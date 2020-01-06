963c964ea8d52fe68755daad8cecbc869e1f2281
Adrian Strugala <strugala.adrian@gmail.com>
2020-01-06 19:52:51 +0100

    added test for connection string in configuration

---
+++ tests/DatabaseDataTests/Configuration/DatabaseDataConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿using DreamTravel.DatabaseData.Configuration;
+using Xunit;
+
+namespace DreamTravel.DatabaseDataTests.Configuration
+{
+    public class DatabaseDataConfigurationTests
+    {
+        private readonly DatabaseDataConfiguration _sut;
+
+        public DatabaseDataConfigurationTests()
+        {
+            _sut = new DatabaseDataConfiguration();
+        }
+
+        [Fact]
+        public void ConnectionString_ContainsReferenceToDemoDatabase()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+            Assert.Contains("dreamtravel-demo", _sut.ConnectionString);
+        }
+    }
+}
