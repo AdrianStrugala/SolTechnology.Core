45f7b4fa86057cd3f003c3be4320c8e9bbbc2772
Adrian Strugala <strugala.adrian@gmail.com>
2020-01-07 19:52:29 +0100

    fixing problems with build

---
+++ src/Backend/Api/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreamTravel.Api
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return OkObjectResult
+            return OkObjectResult("Dream Travels works!");
+        }
+    }
+}
