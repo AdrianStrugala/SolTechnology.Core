ad2e29b725171546babdb431bec783a7fc64de09
Adrian <strugala.adrian@gmail.com>
2020-01-07 15:11:53 +0100

    amend

--- src/Frontend/src/app/dream-trips/city.service.ts
+++ src/Frontend/src/app/dream-trips/city.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { DisplayService } from "./display.service";
 import { FormGroup, FormControl } from "@angular/forms";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
