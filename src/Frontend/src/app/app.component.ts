5d51bb5df8bc93685331f1e081dd7b11aa2a38d7
Adrian <strugala.adrian@gmail.com>
2020-01-09 14:30:39 +0100

    added title to application

--- src/Frontend/src/app/app.component.ts
+++ src/Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css', './external-libraries/bootstrap.css']
 })
 export class AppComponent {
-  title = 'app';
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle("Dream Travels");
+  }
+
 }
