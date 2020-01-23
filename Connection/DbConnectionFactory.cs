ca2cbf5bdcc51f73216a11899e9d44aa97e9bc06
Adrian <strugala.adrian@gmail.com>
2020-01-23 08:36:33 +0100

    added DbConnectionFactory

---
+++ Connection/DbConnectionFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Polly;
+
+namespace SolTechnology.Database.Connection
+{
+    public class DbConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+        private readonly Random _random = new Random();
+
+        public DbConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            Policy.Handle<Exception>()
+                .WaitAndRetry(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt)) // 3,9,27s
+                               + TimeSpan.FromMilliseconds(_random.Next(1000))) //delay up to 1s
+                .Execute(() =>
+                  {
+                      connection.Open();
+                  });
+
+            return connection;
+        }
+
+    }
+}
---
+++ Connection/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace SolTechnology.Database.Connection
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection CreateConnection();
+    }
+}
