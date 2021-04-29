ef75cf48a059806f69323c349caeed14e981a59a
AdrianStrugala <strugala.adrian@gmail.com>
2021-04-29 09:23:00 +0200

    dont know what Im doing xd

--- backend/tests/DatabaseDataTests/TestsConfiguration/SqlFixture.cs
+++ backend/tests/DatabaseDataTests/TestsConfiguration/SqlFixture.cs
@@ -16,7 +16,7 @@ namespace DreamTravel.DatabaseDataTests.TestsConfiguration
         public async Task InitializeAsync()
         {
             _connectionString =
-                "Data Source=localhost,5433;Database=DreamTravelDatabase;User ID=SA;Password=password_xxddd_2137;Persist Security Info=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
+                "Data Source=localhost,1433;Database=DreamTravelDatabase;Integrated Security=SSPI;persist security info=True;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
             DbConnectionFactory = new DbConnectionFactory(_connectionString);
 
             SqlConnection?.Dispose();
