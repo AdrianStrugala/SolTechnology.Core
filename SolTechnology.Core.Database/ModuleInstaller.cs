80c88f19db3c6a3e5373a3c058d23a11241aa82a
AdrianStrugala <strugala.adrian@gmail.com>
2021-12-17 10:08:53 +0100

    Module installer for DB works

--- SolTechnology.Core.Database/ModuleInstaller.cs
+++ SolTechnology.Core.Database/ModuleInstaller.cs
@@ -1,29 +1,29 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SolTechnology.Database.Connection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SolTechnology.Core.Database
 {
     public static class ModuleInstaller
     {
-        public static IServiceCollection AddSql(this IServiceCollection services, IConfiguration configuration, SqlConfiguration sqlConfiguration = null)
+        public static IServiceCollection AddSql(this IServiceCollection services, SqlConfiguration? sqlConfiguration = null)
         {
 
             services
                 .AddOptions<SqlConfiguration>()
                 .Configure<IConfiguration>((config, configuration) =>
            {
+
                if (sqlConfiguration == null)
                {
-                   sqlConfiguration = configuration.GetRequiredSection("Configuration:Sql").Get<SqlConfiguration>();
+                   sqlConfiguration = configuration.GetSection("Configuration:Sql").Get<SqlConfiguration>();
                }
-               config = sqlConfiguration;
+
+               if (sqlConfiguration == null)
+               {
+                   throw new ArgumentException($"The [{nameof(SqlConfiguration)}] is missing. Provide it by parameter or configuration section");
+               }
+
+               config.ConnectionString = sqlConfiguration.ConnectionString;
            });
 
             return services;
---
+++ SolTechnology.Core.ExampleApp/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Options;
+using SolTechnology.Core.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//Custom services
+
+builder.Services.AddSql();
+
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
