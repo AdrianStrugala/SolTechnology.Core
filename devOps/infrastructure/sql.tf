16f127dfdf7a5b7694a1c16430d38d2e6b89f0f4
AdrianStrugala <strugala.adrian@gmail.com>
2022-02-21 12:52:18 +0100

    added sql to terraform

---
+++ devOps/infrastructure/sql.tf
@@ -0,0 +1,21 @@
+resource "azurerm_sql_server" "sqlserver" {
+  name                         = "talecodesqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "sa"
+  administrator_login_password = "password_xxddd_2137"
+
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name           = "TaleCode"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+}
