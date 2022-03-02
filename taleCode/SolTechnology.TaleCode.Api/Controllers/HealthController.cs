eabb8fc3ef9e5ab6c5e32f66692f383af3e21ce4
AdrianStrugala <strugala.adrian@gmail.com>
2022-03-02 10:48:47 +0100

    app insights not showing logs

--- taleCode/SolTechnology.TaleCode.Api/Controllers/HealthController.cs
+++ taleCode/SolTechnology.TaleCode.Api/Controllers/HealthController.cs
@@ -6,9 +6,17 @@ namespace SolTechnology.TaleCode.Api.Controllers
     [Route("")]
     public class HealthController : ControllerBase
     {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet(Name = "GetHealth")]
         public string GetAsync()
         {
+            _logger.LogWarning("Run");
             return "I'm Alive!";
         }
     }
