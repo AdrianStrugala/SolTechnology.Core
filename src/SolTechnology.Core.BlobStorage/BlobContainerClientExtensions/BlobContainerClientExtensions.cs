a93df2cd41869ec69cef24b938d75a939ce31142
AdrianStrugala <strugala.adrian@gmail.com>
2022-03-30 10:44:39 +0200

    fixing bug with metadata key

--- src/SolTechnology.Core.BlobStorage/BlobContainerClientExtensions/BlobContainerClientExtensions.cs
+++ src/SolTechnology.Core.BlobStorage/BlobContainerClientExtensions/BlobContainerClientExtensions.cs
@@ -5,7 +5,7 @@ namespace SolTechnology.Core.BlobStorage.BlobContainerClientExtensions
 {
     public static class BlobContainerClientExtensions
     {
-        private const string ContentTypeKey = "Content-Type";
+        private const string SerializationFormatKey = "SerializationFormat";
 
         public static async Task<T> ReadFromBlob<T>(
             this BlobContainerClient client,
@@ -16,14 +16,14 @@ namespace SolTechnology.Core.BlobStorage.BlobContainerClientExtensions
 
             var properties = await blob.GetPropertiesAsync();
             var metadata = properties.Value.Metadata ?? new Dictionary<string, string>();
-            metadata.TryGetValue(ContentTypeKey, out var contentType);
+            metadata.TryGetValue(SerializationFormatKey, out var contentType);
 
             switch (contentType)
             {
-                case "application/avro":
+                case "avro":
                     return AvroConvert.Deserialize<T>(content.Value.Content.ToArray());
 
-                case "application/json":
+                case "json":
                 default:
                     return content.Value.Content.ToObjectFromJson<T>();
             }
@@ -60,8 +60,7 @@ namespace SolTechnology.Core.BlobStorage.BlobContainerClientExtensions
             var properties = await blob.GetPropertiesAsync();
 
             var metadata = properties.Value.Metadata ?? new Dictionary<string, string>();
-            metadata[ContentTypeKey] = $"application/{serializationFormat.ToString().ToLowerInvariant()}";
-
+            metadata[SerializationFormatKey] = $"{serializationFormat.ToString().ToLowerInvariant()}";
 
             await blob.UploadAsync(new MemoryStream(serializedContent), true, CancellationToken.None);
             await blob.SetMetadataAsync(metadata);
