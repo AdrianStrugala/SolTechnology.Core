22b1bfb877ba698d96dcebc71a5f04dd91bff94b
Adrian Strugala <strugala.adrian@gmail.com>
2022-10-26 15:58:34 +0200

    added chain builders

--- src/SolTechnology.Core.CQRS/Chain.cs
+++ src/SolTechnology.Core.CQRS/Chain.cs
@@ -14,4 +14,17 @@
             Value = value;
         }
     }
+
+    public class ChainBuilders
+    {
+        public Chain<T> Chain<T>(Func<T> func)
+        {
+            return new Chain<T>(func());
+        }
+
+        public async Task<Chain<T>> Chain<T>(Func<Task<T>> func)
+        {
+            return new Chain<T>(await func());
+        }
+    }
 }
