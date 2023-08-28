b15d9c4cdfdcbb29c76058a2f8c2ded9347a8dfc
AdrianStrugala <strugala.adrian@gmail.com>
2023-08-28 20:55:14 +0200

    fix

--- sample-tale-code-app/backend/src/Hosts/Api/Configuration/ConfigurationResolver.Prod.cs
+++ sample-tale-code-app/backend/src/Hosts/Api/Configuration/ConfigurationResolver.Prod.cs
@@ -10,7 +10,7 @@ namespace DreamTravel.Api.Configuration
             {
                 SqlDatabaseConfiguration = new SqlDatabaseConfiguration
                 {
-                    ConnectionString = "Server=tcp:dreamtravel.database.windows.net,1433;Initial Catalog=dreamtravel;Persist Security Info=False;User ID=adrian;Password=P4ssw0rd@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;"
+                    ConnectionString = ""
                 }
             };
 
