0e5f4d8ee011b9a8538519ccb593f18635de6623
AdrianStrugala <strugala.adrian@gmail.com>
2023-09-05 14:01:59 +0200

    fix

--- src/SolTechnology.Core.CQRS/ModuleInstaller.cs
+++ src/SolTechnology.Core.CQRS/ModuleInstaller.cs
@@ -7,27 +7,27 @@ namespace SolTechnology.Core.CQRS
     {
         public static IServiceCollection RegisterCommands(this IServiceCollection services)
         {
-            services.RegisterAllImplementations(typeof(ICommandHandler<>));
-            services.RegisterAllImplementations(typeof(ICommandHandler<,>));
+            services.RegisterAllImplementations(typeof(ICommandHandler<>), Assembly.GetCallingAssembly());
+            services.RegisterAllImplementations(typeof(ICommandHandler<,>), Assembly.GetCallingAssembly());
 
             return services;
         }
 
         public static IServiceCollection RegisterQueries(this IServiceCollection services)
         {
-            services.RegisterAllImplementations(typeof(IQueryHandler<,>));
+            services.RegisterAllImplementations(typeof(IQueryHandler<,>), Assembly.GetCallingAssembly());
 
             return services;
         }
 
-        private static IServiceCollection RegisterAllImplementations(this IServiceCollection services, Type genericInterface)
+        private static IServiceCollection RegisterAllImplementations(this IServiceCollection services, Type genericInterface, Assembly assembly)
         {
             if (!genericInterface.IsInterface || !genericInterface.IsGenericType)
             {
                 throw new ArgumentException("Invalid generic interface type", nameof(genericInterface));
             }
 
-            var types = Assembly.GetCallingAssembly()
+            var types = assembly
                 .GetExportedTypes()
                 .Where(t => t.IsClass && !t.IsAbstract)
                 .Select(t => new
