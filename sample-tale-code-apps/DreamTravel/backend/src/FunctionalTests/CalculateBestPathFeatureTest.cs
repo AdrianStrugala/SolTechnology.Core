25fb886c52c73e668f81dff8f23c2c1c9b63a67b
AdrianStrugala <strugala.adrian@gmail.com>
2023-11-08 08:58:29 +0100

    working on new framework for functional tests

---
+++ sample-tale-code-apps/DreamTravel/backend/src/FunctionalTests/CalculateBestPathFeatureTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Xbehave;
+using Xunit;
+using AutoFixture;
+using DreamTravel.FunctionalTests.Models;
+using DreamTravel.FunctionalTests.TestsConfiguration;
+using DreamTravel.TestFixture.Api.TestsConfiguration;
+using DreamTravel.Trips.Domain.Cities;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace DreamTravel.FunctionalTests
+{
+    [Collection(nameof(ApiFunctionalTests))]
+    public class CalculateBestPathFeatureTest
+    {
+        private readonly ApiFixture _apiFixture;
+        private readonly Fixture _fixture;
+
+        public CalculateBestPathFeatureTest(ApiFixture apiFixture)
+        {
+            _apiFixture = apiFixture;
+            _fixture = new Fixture();
+        }
+
+
+        [Scenario(DisplayName = "Search for 3 cities and calculate best path")]
+        public void Register_Login_CreateOrder_GetOrder(List<City> cities)
+        {
+            "Given is list of cities".x(() =>
+            {
+                cities = new List<City>
+                {
+                    new() { Name = "Wroclaw" },
+                    new() { Name = "Vienna" },
+                    new() { Name = "Barcelona" }
+                };
+            });
+
+            "When User searches for location of each of them".x(async () =>
+            {
+                foreach (var city in cities)
+                {
+                    var apiResponse = await _apiFixture.InternalApiIntegrationTestsFixture.PostAsync<City>($"/api/FindLocationOfCity", new { Name = city.Name });
+                    Assert.Equal(HttpStatusCode.OK, apiResponse.HttpStatusCode);
+                }
+
+
+
+            });
+
+            "When User is Logged In".x(async () =>
+            {
+                // var loginResponse = await _apiFixture.InternalApiIntegrationTestsFixture.PostAsync<User>($"/api/users/login", user);
+
+                // user = loginResponse.GetBody();
+                // Assert.Equal(HttpStatusCode.OK, loginResponse.HttpStatusCode);
+                // Assert.NotEqual(Guid.Empty, user.UserId);
+            });
+
+        }
+    }
+}
