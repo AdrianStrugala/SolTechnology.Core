34f1d2aae2f77f79b8371f933646821fc87e17c8
AdrianStrugala <strugala.adrian@gmail.com>
2024-09-22 12:23:48 +0200

    moved tests to dedicated folder

---
+++ sample-tale-code-apps/DreamTravel/backend/src/Infrastructure/DreamTravelDatabase/dbo/City.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE [dbo].[City]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL,
+    [Country] NVARCHAR(50) NOT NULL,
+    [Region] NVARCHAR(50) NOT NULL,
+	[Population] INT NULL,
+	[Latitude] FLOAT NOT NULL,
+	[Longtitude] FLOAT NOT NULL,
+
+	[CreatedAt] datetime2 NOT NULL DEFAULT GETUTCDATE(),
+	[UpdatedAt] datetime2 NULL
+)
+GO
+
+CREATE UNIQUE INDEX [UX_City_UserId] ON [dbo].[City] ([CityId])
+GO
+
+
+CREATE TRIGGER [OnUpdateCity_SetUpdatedAtToCurrentTime]
+ON dbo.[City]
+AFTER UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	UPDATE
+		dbo.[City]
+	SET
+		[UpdatedAt] = GETUTCDATE()
+	FROM
+		dbo.[City]
+		JOIN inserted ON inserted.Id = [City].Id
+
+END
