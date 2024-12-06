2e124405d874909b8e252e6eac179e3cdedf5ad9
Adrian Strugala <strugala.adrian@gmail.com>
2024-12-06 08:41:37 +0100

    fix for Logging

--- src/SolTechnology.Core.Logging/Middleware/LoggingMiddleware.cs
+++ src/SolTechnology.Core.Logging/Middleware/LoggingMiddleware.cs
@@ -20,7 +20,8 @@ public class LoggingMiddleware
         _logger = logger;
         var asyncStopwatch = new AsyncStopwatch();
 
-        using (await AddRequestIdsToScope(context))
+        //cannot put await to using. It causes logger scope to be disposed
+        using (AddRequestIdsToScope(context))
         {
             _logger.LogInformation("Started request: [{RequestMethod} {RequestPath}]",
                 context.Request.Method, context.Request.Path);
@@ -37,7 +38,7 @@ public class LoggingMiddleware
         }
     }
 
-    private async Task<IDisposable> AddRequestIdsToScope(HttpContext context)
+    private IDisposable AddRequestIdsToScope(HttpContext context)
     {
         //To show how can some id's be extracted from query
         context.Request.Query.TryGetValue("userId", out var userId);
@@ -46,8 +47,7 @@ public class LoggingMiddleware
         context.Request.RouteValues.TryGetValue("category", out var category);
 
         //To show how can some id's be extracted from body
-        context.Request.EnableBuffering();
-        var body = await ReadRequestBody(context.Request);
+        var body = ReadRequestBody(context.Request);
 
         string? name = null;
         if (body is not null)
@@ -61,7 +61,7 @@ public class LoggingMiddleware
 
         var disposable = new DisposableCollection
         {
-            _logger.AddToScope("environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")),
+            _logger.AddToScope("environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "unknown"),
             _logger.AddToScope("correlationId", Guid.NewGuid().ToString()),
             _logger.AddToScope("userId", userId.ToString() ?? "unknown"),
             _logger.AddToScope("category", category?.ToString() ?? "unknown"),
@@ -71,14 +71,15 @@ public class LoggingMiddleware
         return disposable;
     }
 
-    private async Task<JsonDocument?> ReadRequestBody(HttpRequest request)
+    private JsonDocument? ReadRequestBody(HttpRequest request)
     {
+        request.EnableBuffering();
         request.Body.Position = 0;
 
         try
         {
             using var reader = new StreamReader(request.Body, Encoding.UTF8, leaveOpen: true);
-            var bodyAsText = await reader.ReadToEndAsync();
+            var bodyAsText = reader.ReadToEnd();
             var document = JsonDocument.Parse(bodyAsText);
             return document;
         }
