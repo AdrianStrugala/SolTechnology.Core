891f1d0a99a383de35dc8f2d333145ff45ef073e
AdrianStrugala <strugala.adrian@gmail.com>
2025-04-24 11:48:57 +0200

    są toggle markerów

--- sample-tale-code-apps/DreamTravel/backend/src/Presentation/DreamTravel.Ui/wwwroot/mapInterop.js
+++ sample-tale-code-apps/DreamTravel/backend/src/Presentation/DreamTravel.Ui/wwwroot/mapInterop.js
@@ -4,26 +4,36 @@
             document.getElementById('map'),
             { center: { lat, lng }, zoom }
         );
+        window._markers = [];
+        window._polylines = [];
     },
-
     drawIntersection: (lat, lng, title) => {
-        new google.maps.Marker({
+        const m = new google.maps.Marker({
             position: { lat, lng },
             map: window._map,
-            title: title
+            title
         });
+        window._markers.push(m);
     },
-
-    drawStreet: (fromLat, fromLng, toLat, toLng, color = "#FF0000") => {
-        new google.maps.Polyline({
+    drawStreet: (fromLat, fromLng, toLat, toLng, color = '#FF0000') => {
+        const line = new google.maps.Polyline({
+            map: window._map,
             path: [
                 { lat: fromLat, lng: fromLng },
                 { lat: toLat, lng: toLng }
             ],
-            map: window._map,
             strokeColor: color,
             strokeOpacity: 0.8,
             strokeWeight: 2
         });
+        window._polylines.push(line);
+    },
+    clearMarkers: () => {
+        window._markers.forEach(m => m.setMap(null));
+        window._markers = [];
+    },
+    clearStreets: () => {
+        window._polylines.forEach(l => l.setMap(null));
+        window._polylines = [];
     }
 };
